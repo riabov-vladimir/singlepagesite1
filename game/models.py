0cde8c0222a664f49cc08632d25a28d18ec20713
wriabow <damnmeandyou@gmail.com>
2022-02-17 15:41:02 +0300

    update game_index

--- game/models.py
+++ game/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class GameState(models.Model):
+	session_key = models.CharField(max_length=500, unique=True)
+	cell1 = models.IntegerField(default=0)
+	cell2 = models.IntegerField(default=0)
+	cell3 = models.IntegerField(default=0)
+	cell4 = models.IntegerField(default=0)
+	cell5 = models.IntegerField(default=0)
+	cell6 = models.IntegerField(default=0)
+	cell7 = models.IntegerField(default=0)
+	cell8 = models.IntegerField(default=0)
+	cell9 = models.IntegerField(default=0)
+	created = models.DateTimeField(auto_now_add=True, verbose_name='Date created')
+	updated = models.DateTimeField(auto_now=True, verbose_name='Date updated')
+
+
+	class Meta:
+		ordering = ('-created',)
+		verbose_name = 'Game state'
+		verbose_name_plural = 'Game states by sessions'
+
+	def __str__(self):
+		return f'Game state for session {self.session_key}'
--- game/views.py
+++ game/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from globals import move
+# from globals import move
+from game.game3x3.Moves import Move
+from game.models import GameState
+from django.core.exceptions import ObjectDoesNotExist
+"""
+Короче действуем так:
 
+При вызове вьюшки смотрим в базе есть ли модель GameState с полем session_key равным 
+собственно ключу конкретной сессии под которой пользователь эту вьюшку вызвал.
+1) Если есть, то пользуемся значениями из данной модельки.
+2) Если нет, то создаем новый экземпляр и работаем с ним. Таким образом дальнейший ход игры будет всегда обращаться
+к пунтку 1.
+"""
 
 def game_action(request, action):
+
+	try:
+		game_state = GameState.objects.get(session_key=request.session.session_key)
+		move = Move(game_state=game_state)
+	except ObjectDoesNotExist:
+		game_state = GameState.objects.create(session_key=request.session.session_key)
+		move = Move(game_state=game_state)
+		move.generate_tile_on_move()
+		move.generate_tile_on_move()
+
 	if action == 1:
 		move.action_left()
 	elif action == 2:
@@ -20,6 +41,33 @@ def game_action(request, action):
 		move.generate_tile_on_move()
 		move.generate_tile_on_move()
 
+	record_move = GameState.objects.get(session_key=request.session.session_key)
+	a = record_move
+	record_move.cell1 = int(move.pattern[0][0]),
+	record_move.cell2 = int(move.pattern[0][1]),
+	record_move.cell3 = int(move.pattern[0][2]),
+	record_move.cell4 = int(move.pattern[1][0]),
+	record_move.cell5 = int(move.pattern[1][1]),
+	record_move.cell6 = int(move.pattern[1][2]),
+	record_move.cell7 = int(move.pattern[2][0]),
+	record_move.cell8 = int(move.pattern[2][1]),
+	record_move.cell9 = int(move.pattern[2][2])
+
+	record_move.save()
+
+	game_state.cell1 = move.pattern[0][0]
+	game_state.cell2 = move.pattern[0][1]
+	game_state.cell3 = move.pattern[0][2]
+	game_state.cell4 = move.pattern[1][0]
+	game_state.cell5 = move.pattern[1][1]
+	game_state.cell6 = move.pattern[1][2]
+	game_state.cell7 = move.pattern[2][0]
+	game_state.cell8 = move.pattern[2][1]
+	game_state.cell9 = move.pattern[2][2]
+
+	game_state.save()
+
+
 	context = {
 		'cell1': move.pattern[0][0],
 		'cell2': move.pattern[0][1],
@@ -29,14 +77,84 @@ def game_action(request, action):
 		'cell6': move.pattern[1][2],
 		'cell7': move.pattern[2][0],
 		'cell8': move.pattern[2][1],
-		'cell9': move.pattern[2][2],
+		'cell9': move.pattern[2][2]
 	}
 	return render(request,
 	              'game/game.html',
 	              context=context)
+
+
+# def game_index(request):
+# 	"""
+# 	obj, created = Person.objects.get_or_create(first_name='John', last_name='Lennon',
+# 	                  defaults={'birthday': date(1940, 10, 9)})
+# 	"""
+# 	try:
+# 		game_state = GameState.objects.get(session_key=request.session.session_key)
+# 		move = Move(game_state=game_state)
+# 	except ObjectDoesNotExist:
+# 		game_state = GameState.objects.create(session_key=request.session.session_key)
+# 		move = Move(game_state=game_state)
+# 		move.generate_tile_on_move()
+# 		move.generate_tile_on_move()
+#
+# 	context = {
+# 		'cell1': move.pattern[0][0],
+# 		'cell2': move.pattern[0][1],
+# 		'cell3': move.pattern[0][2],
+# 		'cell4': move.pattern[1][0],
+# 		'cell5': move.pattern[1][1],
+# 		'cell6': move.pattern[1][2],
+# 		'cell7': move.pattern[2][0],
+# 		'cell8': move.pattern[2][1],
+# 		'cell9': move.pattern[2][2],
+# 	}
+#
+# 	return render(request,
+# 	              'game/game.html',
+# 	              context=context)
 
 
 def game_index(request):
+	"""
+	obj, created = Person.objects.get_or_create(first_name='John', last_name='Lennon',
+	                  defaults={'birthday': date(1940, 10, 9)})
+	"""
+
+	if not request.session.session_key:
+		request.session.create()
+	session_id = request.session.session_key
+
+	try:
+		game_state = GameState.objects.get(session_key=request.session.session_key)
+		object_exists = True
+	except ObjectDoesNotExist:
+		object_exists = False
+
+	if object_exists:
+		move = Move()
+
+		move.pattern = [
+			[game_state.cell1, game_state.cell2, game_state.cell3],
+			[game_state.cell4, game_state.cell5, game_state.cell6],
+			[game_state.cell7, game_state.cell8, game_state.cell9]
+		]
+		print(move)
+	else:
+		game_state = GameState.objects.create(session_key=request.session.session_key)
+		move = Move()
+		print(move)
+		game_state.cell1 = move.pattern[0][0]
+		game_state.cell2 = move.pattern[0][1]
+		game_state.cell3 = move.pattern[0][2]
+		game_state.cell4 = move.pattern[1][0]
+		game_state.cell5 = move.pattern[1][1]
+		game_state.cell6 = move.pattern[1][2]
+		game_state.cell7 = move.pattern[2][0]
+		game_state.cell8 = move.pattern[2][1]
+		game_state.cell9 = move.pattern[2][2]
+
+		game_state.save()
 
 	context = {
 		'cell1': move.pattern[0][0],
@@ -49,8 +167,7 @@ def game_index(request):
 		'cell8': move.pattern[2][1],
 		'cell9': move.pattern[2][2],
 	}
+
 	return render(request,
 	              'game/game.html',
 	              context=context)
-
-
