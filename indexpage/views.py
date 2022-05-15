2b6d4d5bbfb41f36a61e7bb786feff09af003812
riabov-vladimir <vriabovsvet@gmail.com>
2022-05-15 04:05:48 +0300

    add cv

--- indexpage/views.py
+++ indexpage/views.py
@@ -3,28 +3,35 @@ from django.views.generic.base import TemplateView
 
 
 class CatView(TemplateView):
-	template_name = 'indexpage/cat.html'
+    template_name = 'indexpage/cat.html'
 
-	def get_context_data(self):
-		context = {'is_index': False}
-		return context
+    def get_context_data(self):
+        context = {'is_index': False}
+        return context
 
 
 class IndexView(TemplateView):
-	template_name = 'indexpage/index.html'
-	now = str(datetime.datetime.now())
+    template_name = 'indexpage/index.html'
+    now = str(datetime.datetime.now())
 
-	def get_context_data(self):
-		context = {
-			'is_index': True,
-			'now': self.now
-		}
-		return context
+    def get_context_data(self):
+        context = {
+            'is_index': True,
+            'now': self.now
+        }
+        return context
 
 
 class CvView(TemplateView):
-	template_name = 'indexpage/cv.html'
+    template_name = 'indexpage/cv.html'
 
-	def get_context_data(self):
-		context = {'is_index': False}
-		return context
+    def get_age(self):
+        date_1 = '17/05/1988 13:13:08.230010'
+        date_format_str = '%d/%m/%Y %H:%M:%S.%f'
+        diff = datetime.datetime.now() - datetime.datetime.strptime(date_1, date_format_str)
+        return int(diff.total_seconds())
+
+    def get_context_data(self):
+        context = {'is_index': False,
+                   'age': self.get_age()}
+        return context
